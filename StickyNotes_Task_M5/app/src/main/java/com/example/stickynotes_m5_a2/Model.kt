15815cb008326d44a29c064a8a92e3f3cf1c0d6f
harshida7 <141333123+harshida7@users.noreply.github.com>
2023-11-23 17:48:41 +0530

    files

---
+++ StickyNotes_Task_M5/app/src/main/java/com/example/stickynotes_m5_a2/Model.kt
@@ -0,0 +1,12 @@
+package com.example.stickynotes_m5_a2
+
+
+
+
+class Model
+{
+    var id = 0
+    var title= ""
+    var note= ""
+
+}
---
+++ StickyNotes_Task_M5/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "StickyNotes_M5_A2"
+include(":app")
+ 
